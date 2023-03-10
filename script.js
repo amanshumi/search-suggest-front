c0563b0ebcff24f9c81af1fc6a536ec6d168833b
Amanuel <amanuelshumi14@gmail.com>
2023-03-10 04:31:13 -0900

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let result = [];
+
+let filteredData = [];
+
+let elementToUpdate = document.getElementById("results");
+
 document.getElementById('search-input').addEventListener('keyup', async (e) => {
     // Search comments
     // Use this API: https://jsonplaceholder.typicode.com/comments?postId=3
@@ -8,4 +14,38 @@ document.getElementById('search-input').addEventListener('keyup', async (e) => {
     // Use es6
     // Error handling
 
-})
+    let inputVal = e.target.value;
+
+    elementToUpdate.innerHTML = `<p>Loading...</p>`;
+
+    fetch("http://localhost:4000/comments")
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+
+        result = data.data;
+
+        let allElements = ``;
+
+        result = result.filter((resSingle) => {
+            return resSingle.name.includes(inputVal);
+        });
+
+        result.forEach((singleResult) => {
+            allElements += `<li>${singleResult.name}</li>`;
+        });
+
+        elementToUpdate.innerHTML = allElements;
+
+        if(result.length < 1) {
+            elementToUpdate.innerHTML = `<p>No results found</p>`;
+        }
+    })
+    .catch((err) => {
+        elementToUpdate.innerHTML = `<p>Something went wrong. Try again</p>`;
+        console.log(err);
+    })
+
+});
